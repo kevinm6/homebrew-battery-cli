7434ed044bcbe7d7d543a3d3de68e1884589ce90
kevinm6 <72861758+kevinm6@users.noreply.github.com>
2023-03-10 12:25:23 +0100

    update formula with conflicts and fix wrong sha

--- battery-cli.rb
+++ battery-cli.rb
@@ -2,20 +2,32 @@ class BatteryCli < Formula
   desc "CLI for managing the battery charging status"
 
   homepage "https://github.com/kevinm6/battery-cli"
-  url "https://github.com/kevinm6/battery-cli/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "9ba8f211722fa541dbe5ff56d891a69e048ca8abd493f7fd4a0fbe083d872876"
+  url "https://github.com/kevinm6/battery-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "184253c85b5bc90e971f98c74db519cf8960307d713ede34b23035757a3e5a9c"
+  # version "v1.0.2"
   license "MIT"
 
   head "https://github.com/kevinm6/battery-cli.git", branch: "main"
 
-  depends_on "cmake" => :build
-  depends_on "nyx"
+  conflicts_with cask: "battery", because: "both install binaries that can interfere"
 
-  def install
-    system "./setup.sh", *std_configure_args, "--disable-silent-rules"
+  on_macos do
+    on_arm do
+      url "https://github.com/kevinm6/battery-cli/archive/refs/tags/v1.0.2.tar.gz"
+      sha256 "184253c85b5bc90e971f98c74db519cf8960307d713ede34b23035757a3e5a9c"
+
+      def install
+        bin.install "dist/smc"
+        bin.install "battery-cli" => "battery"
+      end
+      # def install
+      #   system "./setup.sh", *std_configure_args, "--disable-silent-rules"
+      # end
+    end
   end
 
   test do
     system "false"
+    # assert_not_equal  system bin/"battery-cli", ""
   end
 end
